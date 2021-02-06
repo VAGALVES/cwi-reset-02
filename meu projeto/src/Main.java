df3fe66e849f118cc59d8c0747d5e401ef5aa672
Vagner Alves <vag_alves@hotmail.com>
2021-02-06 17:50:02 -0300

    Adicionando segundo exercício

--- meu projeto/src/Main.java
+++ meu projeto/src/Main.java
@@ -1,6 +1,20 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Nova alteração!");
+        int x = 6, y = 3;
+        boolean resultado = (x > y);
+
+        System.out.println(+ x + " é maior que " + y +" ? " + resultado);
+
+        int t = 6, z = 3, resto;
+        resto = t % z;
+
+        System.out.println("O resto de " + t + "/" +z +" é: " + resto);
+
+        int a = 6, b = 3;
+        boolean igualdade = (a==b);
+
+        System.out.println(a + " é igual a " + b + " ? " + igualdade);
+
     }
 }
