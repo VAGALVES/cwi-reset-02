ad208c6f6f02d72ad30a53e1728d2f25ceac7671
unknown <vag_alves@hotmail.com>
2021-02-13 12:27:25 -0300

    Exercicio 01 - aula 02

---
+++ exercicio-aula-02/src/exercicio/um/Aplicacao.java
@@ -0,0 +1,18 @@
+package exercicio.um;
+
+public class Aplicacao {
+    public static void main(String[] args) {
+        Diretor diretor1 = new Diretor("Rob Cohen",50,6);
+        Diretor diretor2 = new Diretor("Sam Raimi",45,15);
+
+        Filme filme01 = new Filme("Velozes e Furiosos", "Filme de aventura, vencedor de 2 Oscars", 2001, 130,
+                4,diretor1);
+
+
+        Filme filme02 = new Filme("Spider man", "Melhor filme (2002)", 2002, 105,
+                4,diretor2);
+
+        filme01.exibir();
+        filme02.exibir();
+    }
+}
---
+++ exercicio-aula-02/src/exercicio/um/Filme.java
@@ -0,0 +1,32 @@
+package exercicio.um;
+
+import java.sql.SQLOutput;
+
+public class Filme {
+    private String nome;
+    private String descricao;
+    private int anoDeLancamento;
+    private double duracao;
+    private int avaliacao;
+    private Diretor diretor;
+
+    public Filme(String nome, String descricao, int anoDeLancamento, double duracao, int avaliacao, Diretor diretor) {
+        this.nome = nome;
+        this.descricao = descricao;
+        this.anoDeLancamento = anoDeLancamento;
+        this.duracao = duracao;
+        this.avaliacao = avaliacao;
+        this.diretor = diretor;
+    }
+
+    public void exibir(){
+        System.out.println("Nome do filme: "+this.nome);
+        System.out.println("Descrição: "+this.descricao);
+        System.out.println("Duração: "+this.duracao);
+        System.out.println("Diretor: "+diretor.getNome());
+        System.out.println("");
+    }
+}
+
+
+
