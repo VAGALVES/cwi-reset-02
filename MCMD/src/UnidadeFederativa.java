34751d16bb8ff46b904203c83663c81ebdcc2fe5
unknown <vag_alves@hotmail.com>
2021-02-20 02:45:02 -0300

    Adicionando MCMD

---
+++ MCMD/src/UnidadeFederativa.java
@@ -0,0 +1,40 @@
+public enum UnidadeFederativa {
+
+    AC("Acre"),
+    AL("Alagoas"),
+    AM("Amazonas"),
+    AP("Amapá"),
+    BA("Bahia"),
+    CE("Ceará"),
+    DF("Distrito Federal"),
+    ES("Espíríto Santo"),
+    GO("Goiás"),
+    MA("Maranhão"),
+    MG("Minas Gerais"),
+    MS("Mato Grosso do Sul"),
+    MT("Mato Grosso"),
+    PA("Pará"),
+    PB("Paraíba"),
+    PE("Pernambuco"),
+    PR("Paraná"),
+    PI("Piauí"),
+    RJ("Rio de Janeiro"),
+    RN("Rio Grande do Norte"),
+    RO("Rondônia"),
+    RR("Roraima"),
+    RS("Rio Grande do Sul"),
+    SC("Santa Catarina"),
+    SE("Sergipe"),
+    SP("São Paulo"),
+    TO("Tocantins");
+
+    private String uf;
+
+    UnidadeFederativa(String uf) {
+        this.uf = uf;
+    }
+
+    public String getUf() {
+        return uf;
+    }
+}
